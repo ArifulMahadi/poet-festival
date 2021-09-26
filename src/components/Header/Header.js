00dbe9e5a8db21217243ddd0a22a2254457566a9
ariful2127 <ariful.mahadi@gmail.com>
2021-09-26 22:08:46 +0600

    new commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,8 @@ import './Header.css'
 const Header = () => {
     return (
         <div className='header-text'>
-           <h1>hey!! are you ready to join poet festival </h1>
-           <h3>now im going to show you famous poets</h3>
+           <h1>Hey!! are you ready to join poet festival </h1>
+           <h3>Now im going to show you famous poets</h3>
            <span>there are <strong>100m</strong> people ready to join with us !!</span>
         </div>
     );
